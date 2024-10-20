306dc111a5e65ecf8c84a1e6354bd655e3cc5e95
Raheel Jamshaid <76648311+raheelProjects@users.noreply.github.com>
2024-10-20 12:52:50 +0200

    lesson1
    learned about string interpolation in html and about architecture making my bio data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'stringinterpolation';
+  title: String;
+
+  //just for fun @byRaheel
+  myage: number = 23;
+  firstName: String = 'Raheel';
+  middleName: String = 'Jamshed';
+  lastName: String = 'Minhas';
+  fullName: String = `${this.firstName} ${this.middleName} ${this.lastName}`;
+  fatherName: String = `${this.middleName} Iqbal ${this.lastName}`;
+  birthYear: Number = new Date().getFullYear() - this.myage;
+
+  //proffesional practise
+  constructor() {
+    this.title = 'String Interpolation angular';
+  }
 }
